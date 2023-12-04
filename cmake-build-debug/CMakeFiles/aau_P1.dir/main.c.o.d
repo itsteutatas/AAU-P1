3130070eb88b6a30a32879c391a31c8a7535b83e
wefjyl <AF44KT@student.aau.dk>
2023-12-04 12:38:11 +0100

    Starting fresh in driver.c
    Removed a lot of previous functions to make space for our new approach where each function corresponds to a parameter

--- cmake-build-debug/CMakeFiles/aau_P1.dir/main.c.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/aau_P1.dir/main.c.o: \
-  /Users/hellaarman/Documents/GitHub/aau-P1/main.c \
-  /Users/hellaarman/Documents/GitHub/aau-P1/./main.h
--- driver.c
+++ driver.c
@@ -2,21 +2,28 @@
 #include "stdlib.h"
 #include "main.h"
 
-void input_parameters(double* temperature, double* level_of_nitrogen, double* secchi_depth){
+void input_parameters(double* temperature, double* salinity, double* secchi_depth){
     printf("Measure the temperature in degrees Celsius, input the value without unit and press enter\n");
     scanf("%lf", temperature);
 
     printf("Measure the secchi depth in centimeter, input the value without unit and press enter\n");
     scanf("%lf", secchi_depth);
 
-    printf("Measure the levels of nitrogen in micrograms per liter, input the value without unit and press enter\n");
-    scanf("%lf", level_of_nitrogen);
+    printf("Measure the salinity in ppt, input the value without unit and press enter\n");
+    scanf("%lf", salinity);
 }
 
-void print_txt(FILE* f, double temperature, double level_of_nitrogen, double secchi_depth){
-    fprintf(f, "%.1lf degrees Celsius | %.1lf micrograms per Liter | %.1lf centimeters\n", temperature, level_of_nitrogen, secchi_depth);
+void save_entry(FILE* f, double temperature, double salinity, double secchi_depth){
+    fprintf(f, "%.1lf degrees Celsius | %.1lf ppt salinity | %.1lf centimeters\n", temperature, salinity, secchi_depth);
 }
 
+//Check which bracket temperature input falls into. Returns: Int corresponding with bracket
+
+//Check the maximum depth limit based on Secchi depth. Returns: Int max depth limit in centimeters, rounded up
+
+//Check which bracket salinity input falls into. Returns: Returns: Int corresponding with bracket
+
+//Print statement for each parameter as a result
 
 int main(){
     double temperature, level_of_nitrogen, secchi_depth;
@@ -28,7 +35,7 @@ int main(){
     FILE *f = fopen("fil1.txt", "w");
     if (f == NULL)
     {
-        printf("filen blev ikke åbnet");
+        printf("The file could not be opened.");
         exit(EXIT_FAILURE);
     }
 
