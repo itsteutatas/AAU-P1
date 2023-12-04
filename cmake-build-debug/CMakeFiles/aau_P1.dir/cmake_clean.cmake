8e6ff0fa5820faae005246d1062fd90e513a40dc
FelixBjerre <gk30jf@student.aau.dk>
2023-12-04 12:42:13 +0100

    Starting over in driver.c

--- cmake-build-debug/CMakeFiles/aau_P1.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/aau_P1.dir/cmake_clean.cmake
@@ -1,12 +1,10 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/aau_P1.dir/driver.c.obj"
-  "CMakeFiles/aau_P1.dir/driver.c.obj.d"
-  "CMakeFiles/aau_P1.dir/main.c.obj"
-  "CMakeFiles/aau_P1.dir/main.c.obj.d"
-  "aau_P1.exe"
-  "aau_P1.exe.manifest"
+  "CMakeFiles/aau_P1.dir/driver.c.o"
+  "CMakeFiles/aau_P1.dir/driver.c.o.d"
+  "CMakeFiles/aau_P1.dir/main.c.o"
+  "CMakeFiles/aau_P1.dir/main.c.o.d"
+  "aau_P1"
   "aau_P1.pdb"
-  "libaau_P1.dll.a"
 )
 
 # Per-language clean rules from dependency scanning.
